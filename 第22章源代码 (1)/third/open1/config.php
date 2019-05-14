ddabf79310753c0d14a452090e3d4591e9fd7878
472164571@qq.com <472164571@qq.com>
2019-05-14 18:09:13 +0800

    s

--- 第22章源代码 (1)/third/open1/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("AppID",             "wx2c864d9d0837f953");
-define("AppSecret",         "9e77e492f3f320afb4b54ef1731159d8");
-
-define("Token",             "weixin");
-define("EncodingAESKey",    "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFG");
-
-?>
